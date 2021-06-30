b75746f842a94c065278e90da0c3263dcd58ba48
dwarf <ixltech.aven@gmail.com>
2021-06-30 21:57:05 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -37,9 +37,9 @@ class RestaurantTest {
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
-//        Restaurant restaurant = Mockito.mock(Restaurant.class);
-//        LocalTime currentTime = LocalTime.parse("23:00:00");
-//        Mockito.when(restaurant.getCurrentTime()).thenReturn(currentTime);
+        Restaurant restaurant = Mockito.mock(Restaurant.class);
+        LocalTime currentTime = LocalTime.parse("23:00:00");
+        Mockito.when(restaurant.getCurrentTime()).thenReturn(currentTime);
         assertFalse(restaurant.isRestaurantOpen());
 
     }
@@ -69,4 +69,26 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //TDD
+
+    //Display the number of items
+    //selected item should have name price with total price.
+    //On removing the price must be subtracted.
+    //show the total price once selected all.
+    //The item should be available in the menu card
+
+    @Test
+    public void when_adding_items_it_should_calculate_total_price(){
+
+        restaurant.addToMenu("Biryani", 1000);
+        restaurant.addToMenu("Butter Chicken", 300);
+        ArrayList<String> bag = new ArrayList<String>();
+        bag.add("Biryani");
+        bag.add("Butter Chicken");
+        assertNotNull(restaurant);
+        assertEquals("Your order will cost ₹1300", restaurant.returnTotalPrice(bag));
+        bag.remove("Butter Chicken");
+        assertEquals("Your order will cost ₹1000", restaurant.returnTotalPrice(bag));
+    }
 }
